dc0f12d92a69dc614318098651dbdd84ab1a7e64
Gabriele Giambonini <gabriele.giambonini@axa.ch>
2021-11-26 09:13:43 +0100

    Cazzate

---
+++ src/main/kotlin/com/fdtheroes/sgruntbot/actions/Cazzate.kt
@@ -0,0 +1,17 @@
+package com.fdtheroes.sgruntbot.actions
+
+import com.fdtheroes.sgruntbot.BotUtils
+import org.telegram.telegrambots.meta.api.objects.Message
+import kotlin.random.Random.Default.nextInt
+
+class Cazzate : Action {
+
+    private val cazzate = listOf("cazzate", "stronzate", "stupidate", "boiate figliolo")
+
+    override fun doAction(message: Message) {
+        if (nextInt(100) == 0) {
+            val cosa = cazzate.random()
+            BotUtils.rispondi(message, "Ma la smetti di dire ${cosa}?");
+        }
+    }
+}
