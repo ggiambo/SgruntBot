60490db01b07deab15fdc3bc8bc0f4d15cf1e196
Gabriele Giambonini <gabriele.giambonini@axa.ch>
2022-01-04 10:57:54 +0100

    Version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 plugins {
     kotlin("jvm") version "1.6.10"
     application
-
+    id("com.github.ben-manes.versions") version "0.40.0"
 }
 
 repositories {
@@ -16,18 +16,18 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin", "kotlin-stdlib-jdk8", "1.6.10")
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.5.2")
-    implementation("org.telegram", "telegrambots", "5.5.0")
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.6.0")
+    implementation("org.telegram", "telegrambots", "5.6.0")
     implementation("org.reflections", "reflections", "0.10.2")
-    implementation("ch.qos.logback", "logback-classic", "1.2.7")
+    implementation("ch.qos.logback", "logback-classic", "1.2.10")
     implementation("org.mariadb.jdbc", "mariadb-java-client", "2.7.4")
-    implementation("org.jetbrains.exposed", "exposed-core", "0.36.1")
-    implementation("org.jetbrains.exposed", "exposed-jdbc", "0.36.1")
-    implementation("org.jetbrains.exposed", "exposed-java-time", "0.36.1")
-    testImplementation("org.junit.platform", "junit-platform-launcher", "1.8.1")
-    testImplementation(platform("org.junit:junit-bom:5.8.1"))
+    implementation("org.jetbrains.exposed", "exposed-core", "0.37.3")
+    implementation("org.jetbrains.exposed", "exposed-jdbc", "0.37.3")
+    implementation("org.jetbrains.exposed", "exposed-java-time", "0.37.3")
+    testImplementation("org.junit.platform", "junit-platform-launcher", "1.8.2")
+    testImplementation(platform("org.junit:junit-bom:5.8.2"))
     testImplementation("org.junit.jupiter", "junit-jupiter")
-    testImplementation("org.assertj", "assertj-core", "3.21.0")
+    testImplementation("org.assertj", "assertj-core", "3.22.0")
     testImplementation("org.mockito.kotlin", "mockito-kotlin", "4.0.0")
 }
 
