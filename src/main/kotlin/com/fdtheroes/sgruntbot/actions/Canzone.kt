5057a3946b2796fa61a82e4c04edae562169a85b
giambo <ggiambo@gmail.com>
2022-01-27 15:09:11 +0100

    Usa "yt-dlp"

--- src/main/kotlin/com/fdtheroes/sgruntbot/actions/Canzone.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/actions/Canzone.kt
@@ -59,7 +59,7 @@ class Canzone : Action, HasHalp {
         val processOutput = ProcessBuilder()
             .command(
                 "sh", "-c",
-                """youtube-dl --restrict-filenames --extract-audio --audio-format mp3 --output "$destDir/%(title)s.mp3" "ytsearch1:$query" --geo-bypass-country IT 2>&1"""
+                """yt-dlp --restrict-filenames --extract-audio --audio-format mp3 --output "$destDir/%(title)s.mp3" "ytsearch1:$query" --geo-bypass-country IT 2>&1"""
             )
             .start()
             .inputStream
