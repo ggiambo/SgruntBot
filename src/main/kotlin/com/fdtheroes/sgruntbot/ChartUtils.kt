0ca6950251ebb583b18d4ad411958568b5c6365f
giambo <ggiambo@gmail.com>
2022-09-14 18:04:37 +0200

    Scheduled stats un pochino piu' belle

---
+++ src/main/kotlin/com/fdtheroes/sgruntbot/ChartUtils.kt
@@ -0,0 +1,40 @@
+package com.fdtheroes.sgruntbot
+
+import org.knowm.xchart.BitmapEncoder
+import org.knowm.xchart.internal.chartpart.Chart
+import org.telegram.telegrambots.meta.api.objects.InputFile
+import java.awt.Color
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import javax.imageio.ImageIO
+
+object ChartUtils {
+
+    val seriesColors = arrayOf(
+        colorFromString("#a6cee3"),
+        colorFromString("#1f78b4"),
+        colorFromString("#b2df8a"),
+        colorFromString("#33a02c"),
+        colorFromString("#fb9a99"),
+        colorFromString("#e31a1c"),
+        colorFromString("#fdbf6f"),
+        colorFromString("#ff7f00"),
+        colorFromString("#cab2d6"),
+        colorFromString("#6a3d9a"),
+    )
+
+    fun getAsInputFile(chart: Chart<*, *>) : InputFile {
+        val image = BitmapEncoder.getBufferedImage(chart)
+        val os = ByteArrayOutputStream()
+        ImageIO.write(image, "png", os)
+        return InputFile(ByteArrayInputStream(os.toByteArray()), "stats.jpg")
+    }
+
+    private fun colorFromString(color: String): Color {
+        val colorAsInt = color
+            .trim()
+            .takeLast(6)
+            .toInt(16)
+        return Color(colorAsInt)
+    }
+}
--- src/main/kotlin/com/fdtheroes/sgruntbot/actions/Stats.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/actions/Stats.kt
@@ -1,10 +1,11 @@
 package com.fdtheroes.sgruntbot.actions
 
 import com.fdtheroes.sgruntbot.BotUtils
+import com.fdtheroes.sgruntbot.ChartUtils
+import com.fdtheroes.sgruntbot.ChartUtils.getAsInputFile
 import com.fdtheroes.sgruntbot.SgruntBot
 import com.fdtheroes.sgruntbot.actions.persistence.Stats
 import com.fdtheroes.sgruntbot.actions.persistence.StatsService
-import org.knowm.xchart.BitmapEncoder
 import org.knowm.xchart.PieChart
 import org.knowm.xchart.style.PieStyler
 import org.knowm.xchart.style.theme.GGPlot2Theme
@@ -14,11 +15,7 @@ import org.telegram.telegrambots.meta.api.methods.ParseMode
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto
 import org.telegram.telegrambots.meta.api.objects.InputFile
 import org.telegram.telegrambots.meta.api.objects.Message
-import java.awt.Color
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
 import javax.annotation.PostConstruct
-import javax.imageio.ImageIO
 
 @Lazy
 @Service
@@ -28,18 +25,6 @@ class Stats(
 ) : Action, HasHalp {
 
     private val regex = Regex("^!stats(.*)\$", RegexOption.IGNORE_CASE)
-    private val seriesColors = arrayOf(
-        colorFromString("#a6cee3"),
-        colorFromString("#1f78b4"),
-        colorFromString("#b2df8a"),
-        colorFromString("#33a02c"),
-        colorFromString("#fb9a99"),
-        colorFromString("#e31a1c"),
-        colorFromString("#fdbf6f"),
-        colorFromString("#ff7f00"),
-        colorFromString("#cab2d6"),
-        colorFromString("#6a3d9a"),
-    )
 
     private val pieChart = PieChart(1024, 768)
 
@@ -48,7 +33,7 @@ class Stats(
         pieChart.styler.theme = GGPlot2Theme()
         pieChart.styler.labelType = PieStyler.LabelType.Value
         pieChart.styler.setSliceBorderWidth(5.0)
-        pieChart.styler.seriesColors = seriesColors
+        pieChart.styler.seriesColors = ChartUtils.seriesColors
     }
 
     override fun doAction(message: Message, sgruntBot: SgruntBot) {
@@ -78,11 +63,11 @@ class Stats(
             sendPhoto.photo = inputFile
             sgruntBot.rispondi(sendPhoto)
         }
-
     }
 
-    fun getStatsInputFile(stats: List<Stats>, chartTitle: String, sgruntBot: SgruntBot): InputFile {
+    private fun getStatsInputFile(stats: List<Stats>, chartTitle: String, sgruntBot: SgruntBot): InputFile {
         val totalMessages = stats.sumOf { it.messages }
+        pieChart.title = chartTitle
         pieChart.seriesMap.clear()
         stats
             .sortedBy { it.messages }
@@ -94,20 +79,8 @@ class Stats(
                 pieChart.addSeries(name, stat.messages)
             }
 
-        pieChart.title = chartTitle
 
-        val image = BitmapEncoder.getBufferedImage(pieChart)
-        val os = ByteArrayOutputStream()
-        ImageIO.write(image, "png", os)
-        return InputFile(ByteArrayInputStream(os.toByteArray()), "stats.jpg")
-    }
-
-    private fun colorFromString(color: String): Color {
-        val colorAsInt = color
-            .trim()
-            .takeLast(6)
-            .toInt(16)
-        return Color(colorAsInt)
+        return getAsInputFile(pieChart)
     }
 
     private fun getPercentage(messages: Int, total: Int): Int {
