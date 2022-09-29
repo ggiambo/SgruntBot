6e9548c813a5ab3ffa9a2037c5c1b2d258a523f6
Giambo <ggiambo@gmail.com>
2022-09-29 15:16:46 +0200

    Sgrunty non redarguisce Giambo

--- src/main/kotlin/com/fdtheroes/sgruntbot/actions/Cazzate.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/actions/Cazzate.kt
@@ -1,19 +1,33 @@
 package com.fdtheroes.sgruntbot.actions
 
 import com.fdtheroes.sgruntbot.SgruntBot
+import com.fdtheroes.sgruntbot.Users
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.meta.api.objects.Message
+import org.telegram.telegrambots.meta.api.objects.User
 import kotlin.random.Random.Default.nextInt
 
 @Service
 class Cazzate : Action {
 
     private val cazzate = listOf("cazzate", "stronzate", "stupidate", "boiate figliolo")
+    private val rispondiGiambo = listOf(
+        "Amen, AMEN!",
+        "Questa è una grande verità",
+        "WOW, non ci avevo mai pensato!"
+    )
 
     override fun doAction(message: Message, sgruntBot: SgruntBot) {
         if (nextInt(200) == 0) {
-            val cosa = cazzate.random()
-            sgruntBot.rispondi(message, "Ma la smetti di dire ${cosa}?")
+            sgruntBot.rispondi(message, getMessaggio(message.from))
+        }
+    }
+
+    fun getMessaggio(from: User): String {
+        if (from.id == Users.GIAMBO.id) {
+            return rispondiGiambo.random()
+        } else {
+            return "Ma la smetti di dire ${cazzate.random()}?"
         }
     }
 }
