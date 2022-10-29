80936532cd2493559e9bcd49c42d32074a23b80a
giambo <ggiambo@gmail.com>
2022-10-29 17:03:15 +0200

    Cazzate Plus

--- src/main/kotlin/com/fdtheroes/sgruntbot/actions/Cazzate.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/actions/Cazzate.kt
@@ -1,33 +1,37 @@
 package com.fdtheroes.sgruntbot.actions
 
+import com.fdtheroes.sgruntbot.BotUtils
 import com.fdtheroes.sgruntbot.SgruntBot
 import com.fdtheroes.sgruntbot.Users
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.meta.api.objects.Message
-import org.telegram.telegrambots.meta.api.objects.User
 import kotlin.random.Random.Default.nextInt
 
 @Service
-class Cazzate : Action {
+class Cazzate(private val botUtils: BotUtils) : Action {
 
     private val cazzate = listOf("cazzate", "stronzate", "stupidate", "boiate figliolo")
     private val rispondiGiambo = listOf(
         "Amen, AMEN!",
         "Questa è una grande verità",
-        "WOW, non ci avevo mai pensato!"
+        "WOW, non ci avevo mai pensato!",
+        "Giambo, tu si che sai parlare bene!",
     )
 
     override fun doAction(message: Message, sgruntBot: SgruntBot) {
         if (nextInt(200) == 0) {
-            sgruntBot.rispondi(message, getMessaggio(message.from))
+            val fromId = message.from.id
+            if (fromId == Users.GIAMBO.id) {
+                sgruntBot.rispondi(message, rispondiGiambo.random())
+            } else {
+                if (nextInt(5) == 0) {
+                    val userName = botUtils.getUserName(sgruntBot.getChatMember(fromId))
+                    sgruntBot.rispondi(message, "Ma chiudi il becco, $userName!")
+                } else {
+                    sgruntBot.rispondi(message, "Ma la smetti di dire ${cazzate.random()}?")
+                }
+            }
         }
     }
 
-    fun getMessaggio(from: User): String {
-        if (from.id == Users.GIAMBO.id) {
-            return rispondiGiambo.random()
-        } else {
-            return "Ma la smetti di dire ${cazzate.random()}?"
-        }
-    }
 }
