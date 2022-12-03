f830a5dd8b7b02f6d0a2b914b61b44cecb055d6d
giambo <ggiambo@gmail.com>
2022-12-03 20:49:50 +0100

    Version upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,14 +8,14 @@ plugins {
     application
     kotlin("jvm") version "1.7.10"
     kotlin("plugin.spring") version "1.7.10"
-    id("org.springframework.boot") version "3.0.0-M5"
-    id("io.spring.dependency-management") version "1.0.14.RELEASE"
+    id("org.springframework.boot") version "3.0.0"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("com.github.ben-manes.versions") version "0.44.0"
 }
 
 repositories {
     mavenLocal()
     mavenCentral()
-    maven { url = uri("https://repo.spring.io/milestone") }
 }
 
 configurations {
@@ -31,17 +31,17 @@ dependencies {
     implementation("com.fasterxml.jackson.module", "jackson-module-kotlin")
     implementation("org.jetbrains.kotlin", "kotlin-reflect")
     implementation("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
-    implementation("org.telegram", "telegrambots", "6.1.0")
+    implementation("org.telegram", "telegrambots", "6.3.0")
     implementation("org.jsoup", "jsoup", "1.15.3")
-    implementation("org.springdoc", "springdoc-openapi-ui", "1.6.11")
-    implementation("org.springdoc", "springdoc-openapi-kotlin", "1.6.11")
-    implementation("org.springdoc", "springdoc-openapi-webmvc-core", "1.6.11")
+    implementation("org.springdoc", "springdoc-openapi-ui", "1.6.13")
+    implementation("org.springdoc", "springdoc-openapi-kotlin", "1.6.13")
+    implementation("org.springdoc", "springdoc-openapi-webmvc-core", "1.6.13")
     implementation("org.knowm.xchart", "xchart", "3.8.2")
     runtimeOnly("org.mariadb.jdbc", "mariadb-java-client")
     runtimeOnly("com.h2database", "h2")
     annotationProcessor("org.springframework.boot", "spring-boot-configuration-processor")
     testImplementation("org.springframework.boot", "spring-boot-starter-test")
-    testImplementation("org.mockito.kotlin", "mockito-kotlin", "4.0.0")
+    testImplementation("org.mockito.kotlin", "mockito-kotlin", "4.1.0")
 }
 
 tasks.withType<KotlinCompile> {
