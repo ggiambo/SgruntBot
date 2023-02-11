d840b4baf722470c67f9367d2ea2957fb0f4a41e
giambo <ggiambo@gmail.com>
2023-02-11 15:31:46 +0100

    Version update, semver plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-group = "fdtheroes"
-version = "1.0.2"
-description = "SgruntBot"
-
 plugins {
     application
     kotlin("jvm") version "1.7.10"
     kotlin("plugin.spring") version "1.7.10"
-    id("org.springframework.boot") version "3.0.1"
+    id("org.springframework.boot") version "3.0.2"
     id("io.spring.dependency-management") version "1.1.0"
-    id("com.github.ben-manes.versions") version "0.44.0"
-    id("com.dipien.semantic-version") version "1.0.0"
+    id("com.github.ben-manes.versions") version "0.45.0"
+    id("com.glovoapp.semantic-versioning") version "1.1.10"
 }
+
 
 repositories {
     mavenLocal()
@@ -32,7 +29,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module", "jackson-module-kotlin")
     implementation("org.jetbrains.kotlin", "kotlin-reflect")
     implementation("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
-    implementation("org.telegram", "telegrambots", "6.4.0")
+    implementation("org.telegram", "telegrambots", "6.5.0")
     implementation("org.jsoup", "jsoup", "1.15.3")
     implementation("org.springdoc", "springdoc-openapi-ui", "1.6.14")
     implementation("org.springdoc", "springdoc-openapi-kotlin", "1.6.14")
