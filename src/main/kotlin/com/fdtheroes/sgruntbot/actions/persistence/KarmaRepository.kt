d7458ac7081d5cc9cece5d8bab3f989e4144b72a
giambo <ggiambo@gmail.com>
2023-06-03 11:44:33 +0200

    Crea una entry per utenti che non esistono ancora

--- src/main/kotlin/com/fdtheroes/sgruntbot/actions/persistence/KarmaRepository.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/actions/persistence/KarmaRepository.kt
@@ -3,7 +3,6 @@ package com.fdtheroes.sgruntbot.actions.persistence
 import org.springframework.data.jdbc.repository.query.Modifying
 import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.CrudRepository
-import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.time.LocalDateTime
 
