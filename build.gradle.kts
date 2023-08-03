f853a8182cd219f5a100545e03cd759736427101
giambo <ggiambo@gmail.com>
2023-08-03 19:16:41 +0200

    Version uppete!

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
-    kotlin("jvm") version "1.7.10"
-    kotlin("plugin.spring") version "1.7.10"
-    id("org.springframework.boot") version "3.0.4"
-    id("io.spring.dependency-management") version "1.1.0"
-    id("com.github.ben-manes.versions") version "0.46.0"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.spring") version "1.9.0"
+    id("org.springframework.boot") version "3.1.2"
+    id("io.spring.dependency-management") version "1.1.2"
+    id("com.github.ben-manes.versions") version "0.47.0"
     id("com.glovoapp.semantic-versioning") version "1.1.10"
 }
 
@@ -30,17 +30,17 @@ dependencies {
     implementation("org.jetbrains.kotlin", "kotlin-reflect")
     implementation("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core")
-    implementation("org.telegram", "telegrambots", "6.5.0")
-    implementation("org.jsoup", "jsoup", "1.15.4")
-    implementation("org.springdoc", "springdoc-openapi-ui", "1.6.15")
-    implementation("org.springdoc", "springdoc-openapi-kotlin", "1.6.15")
-    implementation("org.springdoc", "springdoc-openapi-webmvc-core", "1.6.15")
-    implementation("org.knowm.xchart", "xchart", "3.8.3")
+    implementation("org.telegram", "telegrambots", "6.7.0")
+    implementation("org.jsoup", "jsoup", "1.16.1")
+    implementation("org.springdoc", "springdoc-openapi-ui", "1.7.0")
+    implementation("org.springdoc", "springdoc-openapi-kotlin", "1.7.0")
+    implementation("org.springdoc", "springdoc-openapi-webmvc-core", "1.7.0")
+    implementation("org.knowm.xchart", "xchart", "3.8.4")
     runtimeOnly("org.mariadb.jdbc", "mariadb-java-client")
     runtimeOnly("com.h2database", "h2")
     annotationProcessor("org.springframework.boot", "spring-boot-configuration-processor")
     testImplementation("org.springframework.boot", "spring-boot-starter-test")
-    testImplementation("org.mockito.kotlin", "mockito-kotlin", "4.1.0")
+    testImplementation("org.mockito.kotlin", "mockito-kotlin", "5.0.0")
 }
 
 springBoot {
