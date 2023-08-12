e5b01f610eeffa7289277ed03c411c5938d46442
giambo <ggiambo@gmail.com>
2023-08-12 17:01:33 +0200

    Attacco random

--- src/main/kotlin/com/fdtheroes/sgruntbot/actions/Attacca.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/actions/Attacca.kt
@@ -1,13 +1,17 @@
 package com.fdtheroes.sgruntbot.actions
 
+import com.fdtheroes.sgruntbot.BotUtils
 import com.fdtheroes.sgruntbot.actions.models.ActionContext
 import com.fdtheroes.sgruntbot.actions.models.ActionResponse
 import com.fdtheroes.sgruntbot.actions.persistence.ErrePiGiService
+import com.fdtheroes.sgruntbot.actions.persistence.UsersService
 import org.springframework.stereotype.Service
 
 @Service
 class Attacca(
-    private val errePiGiService: ErrePiGiService
+    private val errePiGiService: ErrePiGiService,
+    private val usersService: UsersService,
+    private val botUtils: BotUtils,
 ) : Action, HasHalp {
 
     override fun doAction(ctx: ActionContext) {
@@ -21,6 +25,17 @@ class Attacca(
             }
             ctx.addResponse(ActionResponse.message(testo))
         }
+        if (message.text == "?attacca") {
+            val difensore = usersService
+                .getAllActiveUsers { ctx.getChatMember(it) }
+                .random()
+
+            val testoAttacco = errePiGiService.attacca(message.from, difensore)
+            val testo = "${botUtils.getUserName(message.from)} ubriaco fradicio tenta di attaccare ${
+                botUtils.getUserName(difensore)
+            }"
+            ctx.addResponse(ActionResponse.message("$testo\n$testoAttacco"))
+        }
     }
 
     private fun getErrePiGiReport(ctx: ActionContext): String {
@@ -31,6 +46,7 @@ class Attacca(
         return "E chi vorresti mai attaccare, grullo?\n\n$testoErrePiGiReport"
     }
 
-    override fun halp() = "<b>!attacca</b> Rispondi a un messaggio con questo testo per attaccare l'autore"
+    override fun halp() =
+        "<b>!attacca</b> Rispondi a un messaggio con questo testo per attaccare l'autore\n<b>?attacca</b> Attacco random."
 
 }
