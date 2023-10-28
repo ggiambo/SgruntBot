afca6b288eb1e7a2c755d006a482dc052fe77168
Giambo <ggiambo@gmail.com>
2023-10-28 13:05:24 +0200

    Cleanup non-existing users (#56)

--- src/main/kotlin/com/fdtheroes/sgruntbot/actions/persistence/UtontiRepository.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/actions/persistence/UtontiRepository.kt
@@ -3,10 +3,14 @@ package com.fdtheroes.sgruntbot.actions.persistence
 import com.fdtheroes.sgruntbot.actions.models.Utonto
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 @Repository
 interface UtontiRepository : JpaRepository<Utonto, Long> {
 
     fun findByUserId(userId: Long): Utonto?
+
+    @Transactional
+    fun deleteAllByUserIdIn(userIds: List<Long>)
 
 }
