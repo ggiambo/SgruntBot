dd40a3a6e0dd2acf7d7146713b4d6ec4d2e58a74
Giambo <ggiambo@gmail.com>
2023-11-16 10:51:27 +0100

    Schduler per i TODOs (#59)

---
+++ src/main/kotlin/com/fdtheroes/sgruntbot/scheduled/fix/ScheduledTodos.kt
@@ -0,0 +1,53 @@
+package com.fdtheroes.sgruntbot.scheduled.fix
+
+import com.fdtheroes.sgruntbot.Bot
+import com.fdtheroes.sgruntbot.BotUtils
+import com.fdtheroes.sgruntbot.actions.models.ActionResponse
+import com.fdtheroes.sgruntbot.actions.models.Todos
+import com.fdtheroes.sgruntbot.actions.persistence.TodosService
+import com.fdtheroes.sgruntbot.scheduled.Scheduled
+import org.springframework.stereotype.Service
+import java.time.DayOfWeek
+import java.time.LocalDateTime
+import java.time.temporal.TemporalAdjusters
+
+@Service
+class ScheduledTodos(
+    private val sgruntBot: Bot,
+    private val botUtils: BotUtils,
+    private val todosService: TodosService,
+) : Scheduled {
+    override fun firstRun() = mezzanotteVenerdiProssimo()
+
+    override fun nextRun() = mezzanotteVenerdiProssimo()
+
+    override fun execute() {
+        val todosByUser = todosService.allTodos(true).groupBy { it.userId }
+        val result = todosByUser.entries.joinToString(separator = "\n") {
+            getTodoOfUser(it.key, it.value)
+        }
+        if (result.isNotEmpty()) {
+            sgruntBot.messaggio(ActionResponse.message("<b>Utenti pigri con todos ancora aperti:</b>\n$result"))
+        }
+    }
+
+    private fun getTodoOfUser(userId: Long, todos: List<Todos>): String {
+        val utente = sgruntBot.getChatMember(userId)
+        val userLink = botUtils.getUserLink(utente)
+        val todosList = todos.joinToString(separator = "\n") {
+            val nr = it.id.toString().padStart(4)
+            val todo = it.todo
+            "- $nr: $todo"
+        }
+        return "$userLink:\n$todosList"
+    }
+
+    private fun mezzanotteVenerdiProssimo(): LocalDateTime {
+        val mezzanotte = LocalDateTime.now().withHour(0)
+            .withMinute(0)
+            .withSecond(0)
+            .withNano(0)
+        return mezzanotte.with(TemporalAdjusters.next(DayOfWeek.FRIDAY))
+    }
+
+}
