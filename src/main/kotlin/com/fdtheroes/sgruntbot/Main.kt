66bf16ad0327f7eec1759576c0f6225024bad4ba
Giambo <ggiambo@gmail.com>
2023-11-19 20:35:09 +0100

    forse...

--- src/main/kotlin/com/fdtheroes/sgruntbot/Main.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/Main.kt
@@ -1,11 +1,16 @@
 package com.fdtheroes.sgruntbot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.ApplicationPidFileWriter
+import org.springframework.boot.fromApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.system.ApplicationPid
 
 @SpringBootApplication
 class Main
 
 fun main(args: Array<String>) {
-    runApplication<Main>(*args)
+    runApplication<Main>(*args) {
+        this.addListeners(ApplicationPidFileWriter("sgruntbot.pid"))
+    }
 }
