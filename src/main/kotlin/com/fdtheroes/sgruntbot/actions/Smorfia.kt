f15b6808ff1838e4799fc97ae9a3bda7a77873d5
Giambo <ggiambo@gmail.com>
2023-12-16 11:53:50 +0100

    Smorfia (#61)
    La smorfia napoletana

---
+++ src/main/kotlin/com/fdtheroes/sgruntbot/actions/Smorfia.kt
@@ -0,0 +1,40 @@
+package com.fdtheroes.sgruntbot.actions
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fdtheroes.sgruntbot.actions.models.ActionContext
+import com.fdtheroes.sgruntbot.actions.models.ActionResponse
+import org.springframework.stereotype.Service
+import kotlin.random.Random.Default.nextInt
+
+@Service
+class Smorfia(val mapper: ObjectMapper) : Action {
+
+    private val regex = Regex("\\b(\\d{1,2})\\b", setOf(RegexOption.IGNORE_CASE, RegexOption.MULTILINE))
+
+    private val smorfia: Map<Int, String> by lazy {
+        mapper.readTree(this::class.java.getResourceAsStream("/smorfia.json"))
+            .associate { Pair(it["n"].asInt(), it["text"].asText()) }
+    }
+
+    override fun doAction(ctx: ActionContext) {
+        if (nextInt(50) == 0) {
+            val numero = getNumero(ctx.message.text)
+            if (numero != null) {
+                val testoSmorfia = smorfia[numero]
+                if (testoSmorfia != null) {
+                    ctx.addResponse(ActionResponse.message("\uD83C\uDDEE\uD83C\uDDF9 $numero: $testoSmorfia \uD83E\uDD0C"))
+                }
+            }
+        }
+    }
+
+    fun getNumero(testo: String): Int? {
+        val numero = regex.find(testo)?.groupValues?.get(1)
+        if (numero != null && numero.toInt() in 1..90) {
+            return numero.toInt()
+        }
+
+        return null
+    }
+
+}
