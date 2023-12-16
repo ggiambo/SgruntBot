3d3be6b6c97b541c4e7d34e5ecff7cd63b994c58
Giambo <ggiambo@gmail.com>
2023-12-16 11:22:57 +0100

    La smorfia napoletana

--- src/main/kotlin/com/fdtheroes/sgruntbot/actions/Smorfia.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/actions/Smorfia.kt
@@ -1,21 +1,19 @@
 package com.fdtheroes.sgruntbot.actions
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.type.TypeFactory
 import com.fdtheroes.sgruntbot.actions.models.ActionContext
 import com.fdtheroes.sgruntbot.actions.models.ActionResponse
 import org.springframework.stereotype.Service
 import kotlin.random.Random.Default.nextInt
 
 @Service
-class Smorfia : Action {
+class Smorfia(val mapper: ObjectMapper) : Action {
 
     private val regex = Regex("\\b(\\d{1,2})\\b", setOf(RegexOption.IGNORE_CASE, RegexOption.MULTILINE))
-    private val smorfiaType = TypeFactory.defaultInstance()
-        .constructMapType(Map::class.java, Int::class.java, String::class.java)
 
     private val smorfia: Map<Int, String> by lazy {
-        ObjectMapper().readValue(this::class.java.getResourceAsStream("/smorfia.json"), smorfiaType)
+        mapper.readTree(this::class.java.getResourceAsStream("/smorfia.json"))
+            .associate { Pair(it["n"].asInt(), it["text"].asText()) }
     }
 
     override fun doAction(ctx: ActionContext) {
@@ -24,7 +22,7 @@ class Smorfia : Action {
             if (numero != null) {
                 val testoSmorfia = smorfia[numero]
                 if (testoSmorfia != null) {
-                    ctx.addResponse(ActionResponse.message(testoSmorfia))
+                    ctx.addResponse(ActionResponse.message("\uD83C\uDDEE\uD83C\uDDF9 $numero: $testoSmorfia \uD83E\uDD0C"))
                 }
             }
         }
