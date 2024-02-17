487ecf333bec47abd50b86d5a469618d1314de0f
Giambo <ggiambo@gmail.com>
2024-02-17 17:10:00 +0100

    Allo scattare della paercentuale

--- src/main/kotlin/com/fdtheroes/sgruntbot/scheduled/random/ScheduledYearProgression.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/scheduled/random/ScheduledYearProgression.kt
@@ -5,8 +5,8 @@ import com.fdtheroes.sgruntbot.actions.YearProgress
 import com.fdtheroes.sgruntbot.actions.models.ActionResponse
 import com.fdtheroes.sgruntbot.scheduled.Scheduled
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 import java.time.LocalDateTime
-import kotlin.random.Random
 
 @Service
 class ScheduledYearProgression(private val sgruntBot: Bot) : Scheduled {
@@ -15,10 +15,21 @@ class ScheduledYearProgression(private val sgruntBot: Bot) : Scheduled {
 
     override fun nextRun() = getNext()
 
-    // random tra 5 e 10 giorni
+    // allo scattare della percentuale
     private fun getNext(): LocalDateTime {
-        val giorni = Random.nextLong(5, 10)
-        return LocalDateTime.now().plusDays(giorni)
+        val onePercentInSeconds = (LocalDate.now().lengthOfYear() * 24 * 60 * 60) / 100.toLong()
+        var next = LocalDateTime.now()
+            .withMonth(1)
+            .withDayOfMonth(1)
+            .withHour(0)
+            .withMinute(0)
+            .withSecond(0)
+            .withNano(0)
+        while (next.isBefore(LocalDateTime.now())) {
+            next = next.plusSeconds(onePercentInSeconds)
+        }
+
+        return next
     }
 
     override fun execute() {
