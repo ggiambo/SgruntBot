05d35662e0d516d93d9163c39fa8a48e93998e56
Giambo <ggiambo@gmail.com>
2024-03-06 18:52:39 +0100

    Version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
     kotlin("jvm") version "1.9.20"
     kotlin("plugin.spring") version "1.9.20"
     kotlin("plugin.jpa") version "1.9.20"
-    id("org.springframework.boot") version "3.1.5"
+    id("org.springframework.boot") version "3.1.9"
     id("io.spring.dependency-management") version "1.1.4"
-    id("com.github.ben-manes.versions") version "0.50.0"
+    id("com.github.ben-manes.versions") version "0.51.0"
     id("com.glovoapp.semantic-versioning") version "1.1.10"
 }
 
@@ -34,7 +34,7 @@ dependencies {
     implementation("org.jetbrains.kotlin", "kotlin-reflect")
     implementation("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core")
-    implementation("org.telegram", "telegrambots", "6.9.7.0")
+    implementation("org.telegram", "telegrambots", "6.9.7.1")
     implementation("org.jsoup", "jsoup", "1.17.2")
     implementation("org.springdoc", "springdoc-openapi-starter-common", "2.3.0")
     implementation("org.springdoc", "springdoc-openapi-starter-webmvc-ui", "2.3.0")
