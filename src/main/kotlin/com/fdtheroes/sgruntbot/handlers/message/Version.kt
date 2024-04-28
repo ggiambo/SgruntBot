fd4204c64f6cfe217142d7e0892f676b21be60d2
Giambo <ggiambo@gmail.com>
2024-04-28 14:59:07 +0200

    Build time

--- src/main/kotlin/com/fdtheroes/sgruntbot/handlers/message/Version.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/handlers/message/Version.kt
@@ -6,12 +6,26 @@ import com.fdtheroes.sgruntbot.utils.BotUtils
 import org.springframework.boot.info.BuildProperties
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.meta.api.objects.Message
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 @Service
-class Version(botUtils: BotUtils, botConfig: BotConfig, buildProperties: BuildProperties) :
+class Version(
+    botUtils: BotUtils,
+    botConfig: BotConfig,
+    buildProperties: BuildProperties,
+) :
     MessageHandler(botUtils, botConfig), HasHalp {
 
-    private val versionString = "${buildProperties.name}: ${buildProperties.version} (${buildProperties.time})"
+    private val versionString: String
+    init {
+        val time = DateTimeFormatter.ISO_DATE_TIME
+            .withZone(ZoneId.of("CET"))
+            .format(buildProperties.time)
+        versionString = "${buildProperties.name}: ${buildProperties.version} ($time)"
+    }
+
     override fun handle(message: Message) {
         if (message.text == "!version") {
             botUtils.rispondi(ActionResponse.message(versionString), message)
