00fda8eff83752af56fa1f803bd6f52a034cab84
Giambo <ggiambo@gmail.com>
2024-06-15 09:15:54 +0200

    Piccoli cambiamenti

--- src/main/kotlin/com/fdtheroes/sgruntbot/BotConfig.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/BotConfig.kt
@@ -2,8 +2,10 @@ package com.fdtheroes.sgruntbot
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
+import org.telegram.telegrambots.longpolling.util.DefaultGetUpdatesGenerator
 import org.telegram.telegrambots.meta.TelegramUrl
 import org.telegram.telegrambots.meta.api.methods.updates.AllowedUpdates
+import org.telegram.telegrambots.meta.api.methods.updates.GetUpdates
 import org.telegram.telegrambots.meta.api.objects.User
 import java.net.InetSocketAddress
 import java.net.Proxy
@@ -20,10 +22,7 @@ class BotConfig(
 
     val botName = "SgruntBot"
     val proxy by lazy { initProxy() }
-    val allowedUpdates = listOf(
-        AllowedUpdates.MESSAGE,
-        "message_reaction",
-    )
+    val allowedUpdates = DefaultGetUpdatesGenerator(listOf(AllowedUpdates.MESSAGE, "message_reaction"))
     val defaultUrl = { TelegramUrl.DEFAULT_URL }
 
     var lastSuper: User? = null
--- src/main/kotlin/com/fdtheroes/sgruntbot/SgruntBot.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/SgruntBot.kt
@@ -8,7 +8,6 @@ import kotlinx.coroutines.launch
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication
-import org.telegram.telegrambots.longpolling.util.DefaultGetUpdatesGenerator
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer
 import org.telegram.telegrambots.meta.api.objects.Update
 
@@ -24,11 +23,10 @@ class SgruntBot(
 
     @PostConstruct
     fun postConstruct() {
-        val botsApplication = TelegramBotsLongPollingApplication()
-        botsApplication.registerBot(
+        TelegramBotsLongPollingApplication().registerBot(
             botConfig.telegramToken,
             botConfig.defaultUrl,
-            DefaultGetUpdatesGenerator(botConfig.allowedUpdates),
+            botConfig.allowedUpdates,
             this
         )
         log.info("Sono partito!")
