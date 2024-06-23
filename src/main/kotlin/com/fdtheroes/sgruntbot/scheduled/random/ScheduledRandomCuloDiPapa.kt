92d41c0f962062e66ff33b9151d7b74da057a3fa
Giambo <ggiambo@gmail.com>
2024-06-23 16:52:08 +0200

    CdP ha rotto le balle

--- src/main/kotlin/com/fdtheroes/sgruntbot/scheduled/random/ScheduledRandomCuloDiPapa.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/scheduled/random/ScheduledRandomCuloDiPapa.kt
@@ -4,6 +4,8 @@ import com.fdtheroes.sgruntbot.models.ActionResponse
 import com.fdtheroes.sgruntbot.utils.BotUtils
 import org.jsoup.Jsoup
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+import kotlin.random.Random.Default.nextLong
 
 @Service
 class ScheduledRandomCuloDiPapa(private val botUtils: BotUtils) : ScheduledRandom {
@@ -30,6 +32,19 @@ class ScheduledRandomCuloDiPapa(private val botUtils: BotUtils) : ScheduledRando
         val text = "${testo}\n${autore}"
 
         botUtils.messaggio(ActionResponse.message(text))
+    }
+
+    // random tra 12 e 16 ore
+    override fun firstRun(): LocalDateTime {
+        val minuti = nextLong(12 * 60, 16 * 60)
+        return LocalDateTime.now().plusMinutes(minuti)
+    }
+
+
+    // random tra 16 e 24 ore
+    override fun nextRun(): LocalDateTime {
+        val minuti = nextLong(16 * 60, 24 * 60)
+        return LocalDateTime.now().plusMinutes(minuti)
     }
 
 }
