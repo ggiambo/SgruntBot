0044f79b613c320665c9fce71df9a5191748858e
Giambo <ggiambo@gmail.com>
2024-08-25 16:47:47 +0200

    Compleanni!

--- src/main/kotlin/com/fdtheroes/sgruntbot/Users.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/Users.kt
@@ -1,16 +1,20 @@
 package com.fdtheroes.sgruntbot
 
-enum class Users(val id: Long) {
+import java.time.LocalDate
+import java.time.Month
+import java.time.Month.*
+
+enum class Users(val id: Long, val compleanno: Compleanno? = null) {
     SPDT(24492275),
-    DANIELE(32657811),
-    SHDX_T(68714652),
-    IL_VINCI(104278889),
-    DA_DA_212(252800958),
-    F(259607683),
-    GGIAMBO(353708759),
-    MALUCAPERDIDA(1475337828),
+    DANIELE(32657811, Compleanno(13, FEBRUARY)),
+    SHDX_T(68714652, Compleanno(11, JULY)),
+    IL_VINCI(104278889, Compleanno(24, AUGUST)),
+    DA_DA_212(252800958, Compleanno(6, AUGUST)),
+    F(259607683, Compleanno(5, APRIL)),
+    GGIAMBO(353708759, Compleanno(9, APRIL)),
+    MALUCAPERDIDA(1475337828, Compleanno(12, AUGUST)),
     BLAH_BANF_BOT(2097709389),
-    ALE(5770928065),
+    ALE(5770928065, Compleanno(25, NOVEMBER)),
     GENGIVA_BOT(6383648928),
     DADUNKEN_BOT(6470939064)
     ;
@@ -18,6 +22,24 @@ enum class Users(val id: Long) {
     companion object {
         private val userIds = entries.associateBy { it.id }
         fun byId(id: Long) = userIds[id]
+        val compleanni = entries
+            .filter { it.compleanno != null }
+            .sortedBy { it.compleanno }
+    }
+}
+
+class Compleanno(val giorno: Int, val mese: Month) : Comparable<Compleanno> {
+    override fun compareTo(other: Compleanno): Int {
+        val meseCompare = mese.compareTo(other.mese)
+        if (meseCompare != 0) {
+            return meseCompare
+        }
+        return giorno.compareTo(other.giorno)
+    }
+
+    fun data(): LocalDate {
+        val now = LocalDate.now()
+        return LocalDate.of(now.year, mese, giorno)
     }
 
 }
---
+++ src/main/kotlin/com/fdtheroes/sgruntbot/handlers/message/Compleanni.kt
@@ -0,0 +1,30 @@
+package com.fdtheroes.sgruntbot.handlers.message
+
+import com.fdtheroes.sgruntbot.BotConfig
+import com.fdtheroes.sgruntbot.Users
+import com.fdtheroes.sgruntbot.models.ActionResponse
+import com.fdtheroes.sgruntbot.utils.BotUtils
+import org.springframework.stereotype.Service
+import org.telegram.telegrambots.meta.api.objects.message.Message
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+@Service
+class Compleanni(botUtils: BotUtils, botConfig: BotConfig) : MessageHandler(botUtils, botConfig), HasHalp {
+    val dataCompleanno = DateTimeFormatter.ofPattern("dd LLLL", Locale.ITALIAN)
+    private val regex = Regex("^!compleanni", RegexOption.IGNORE_CASE)
+
+    override fun handle(message: Message) {
+        if (regex.containsMatchIn(message.text)) {
+            val risposta = Users.compleanni.joinToString(separator = "\n") {
+                val userName = botUtils.getUserName(botUtils.getChatMember(it.id))
+                val data = dataCompleanno.format(it.compleanno!!.data())
+                "\uD83C\uDF82 $userName: $data "
+            }
+            botUtils.rispondi(ActionResponse.message(risposta), message)
+        }
+    }
+
+    override fun halp() =
+        "<b>!compleanni</b> Tutti i cimpleanni conosciuti"
+}
