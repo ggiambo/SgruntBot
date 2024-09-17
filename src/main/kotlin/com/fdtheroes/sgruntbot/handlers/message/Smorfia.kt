b6f29be3568fe19513f43dceb826eaf99129cb55
giambo <ggiambo@gmail.com>
2024-09-17 16:56:48 +0200

    Richiesta smorfia

--- src/main/kotlin/com/fdtheroes/sgruntbot/handlers/message/Smorfia.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/handlers/message/Smorfia.kt
@@ -13,6 +13,7 @@ class Smorfia(botUtils: BotUtils, botConfig: BotConfig, val mapper: ObjectMapper
     MessageHandler(botUtils, botConfig) {
 
     private val regex = Regex("\\b(\\d{1,2})\\b", setOf(RegexOption.IGNORE_CASE, RegexOption.MULTILINE))
+    private val regexRichiesta = Regex("!smorfia \\d{1,2}", setOf(RegexOption.IGNORE_CASE))
 
     private val smorfia: Map<Int, String> by lazy {
         mapper.readTree(this::class.java.getResourceAsStream("/smorfia.json"))
@@ -21,7 +22,7 @@ class Smorfia(botUtils: BotUtils, botConfig: BotConfig, val mapper: ObjectMapper
 
     override fun handle(message: Message) {
         val numero = getNumero(message.text)
-        if (numero != null && nextInt(50) == 0) {   // 2% di probabilità
+        if (numero != null && nextInt(50) == 0 || regexRichiesta.containsMatchIn(message.text)) {   // 2% di probabilità oppure richiesta secca
             val testoSmorfia = smorfia[numero]
             if (testoSmorfia != null) {
                 botUtils.rispondi(
