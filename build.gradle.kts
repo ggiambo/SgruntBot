7dce7b66054e54ee8691ec4e973a57bfea03f75a
Giambo <ggiambo@gmail.com>
2024-10-05 11:05:30 +0200

    Uppete!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,18 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val telegrambotsVersion = "7.10.0"
+val jsoupVersion = "1.18.1"
+val springdocVersion ="2.6.0"
+val xchartVersion = "3.8.8"
+val mockitokotlinVersion = "5.4.0"
 
 plugins {
     application
     kotlin("jvm") version "2.0.0"
     kotlin("plugin.spring") version "2.0.0"
     kotlin("plugin.jpa") version "2.0.0"
-    id("org.springframework.boot") version "3.3.3"
+    id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.6"
     id("com.github.ben-manes.versions") version "0.51.0"
     id("com.glovoapp.semantic-versioning") version "1.1.10"
@@ -35,29 +42,33 @@ dependencies {
     implementation("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core")
     implementation("com.squareup.okhttp3", "okhttp")
-    implementation( "org.telegram", "telegrambots-longpolling", "7.10.0")
-    implementation( "org.telegram", "telegrambots-client", "7.10.0")
-    implementation("org.jsoup", "jsoup", "1.18.1")
-    implementation("org.springdoc", "springdoc-openapi-starter-common", "2.6.0")
-    implementation("org.springdoc", "springdoc-openapi-starter-webmvc-ui", "2.6.0")
-    implementation("org.knowm.xchart", "xchart", "3.8.8")
+    implementation( "org.telegram", "telegrambots-longpolling", telegrambotsVersion)
+    implementation( "org.telegram", "telegrambots-client", telegrambotsVersion)
+    implementation("org.jsoup", "jsoup", jsoupVersion)
+    implementation("org.springdoc", "springdoc-openapi-starter-common", springdocVersion)
+    implementation("org.springdoc", "springdoc-openapi-starter-webmvc-ui", springdocVersion)
+    implementation("org.knowm.xchart", "xchart", xchartVersion)
+
     runtimeOnly("org.mariadb.jdbc", "mariadb-java-client")
     runtimeOnly("com.h2database", "h2")
+    testRuntimeOnly("org.junit.platform", "junit-platform-launcher")
+
     annotationProcessor("org.springframework.boot", "spring-boot-configuration-processor")
+
     testImplementation("org.springframework.boot", "spring-boot-starter-test")
     testImplementation("org.junit.jupiter", "junit-jupiter")
-    testRuntimeOnly("org.junit.platform", "junit-platform-launcher")
-    testImplementation("org.mockito.kotlin", "mockito-kotlin", "5.4.0")
+    testImplementation("org.mockito.kotlin", "mockito-kotlin", mockitokotlinVersion)
 }
 
 springBoot {
     buildInfo()
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
+kotlin {
+    compilerOptions {
+        apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = JvmTarget.JVM_17
     }
 }
 
