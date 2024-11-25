1861387671572fab99d4a499a8e6c7712c83c0d0
Giambo <ggiambo@gmail.com>
2024-11-25 18:19:32 +0100

    Sarru ha sempre ragione (tm)!

--- src/main/kotlin/com/fdtheroes/sgruntbot/scheduled/fix/ScheduledCompleanni.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/scheduled/fix/ScheduledCompleanni.kt
@@ -2,46 +2,19 @@ package com.fdtheroes.sgruntbot.scheduled.fix
 
 import com.fdtheroes.sgruntbot.Users
 import com.fdtheroes.sgruntbot.models.ActionResponse
-import com.fdtheroes.sgruntbot.scheduled.Scheduled
 import com.fdtheroes.sgruntbot.utils.BotUtils
 import org.springframework.stereotype.Service
 import java.time.LocalDate
-import java.time.LocalDateTime
 
 @Service
-class ScheduledCompleanni(private val botUtils: BotUtils) : Scheduled {
-
-    override fun firstRun() = getProssimoCompleanno()
-
-    override fun nextRun() = getProssimoCompleanno()
+class ScheduledCompleanni(private val botUtils: BotUtils) : ScheduledAMezzanotte {
 
     override fun execute() {
-        val prossimoCompleannoUtente = getProssimoCompleannoUtente()
-        val utenteId = prossimoCompleannoUtente.id
-        val user = botUtils.getChatMember(utenteId)
-        val userLink = botUtils.getUserLink(user)
-        botUtils.messaggio(ActionResponse.message("\uD83C\uDF82 Tanti auguri $userLink \uD83C\uDF89 !"))
+        Users.compleanni
+            .filter { it.compleanno?.data() == LocalDate.now() }
+            .map { botUtils.getChatMember(it.id) }
+            .map { botUtils.getUserLink(it) }
+            .map { "\uD83C\uDF82 Tanti auguri $it \uD83C\uDF89 !" }
+            .forEach { botUtils.messaggio(ActionResponse.message(it)) }
     }
-
-    private fun getProssimoCompleannoUtente(): Users {
-        val now = LocalDate.now()
-        val prossimoCompleannoUtente = Users.compleanni.firstOrNull {
-            it.compleanno!!.data().isAfter(now)
-        }
-        if (prossimoCompleannoUtente != null) {
-            return prossimoCompleannoUtente
-        }
-
-        return Users.compleanni.first()
-    }
-
-    private fun getProssimoCompleanno(): LocalDateTime {
-        val prossimoCompleanno = getProssimoCompleannoUtente().compleanno!!.data().atStartOfDay()
-        if (prossimoCompleanno.isAfter(LocalDateTime.now())) {
-            return prossimoCompleanno
-        }
-
-        return prossimoCompleanno.plusYears(1)
-    }
-
 }
