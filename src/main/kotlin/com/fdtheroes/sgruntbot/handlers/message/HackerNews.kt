9e8227acc2e105b2450e8f97877f930a244fe777
Giambo <ggiambo@gmail.com>
2025-04-30 20:50:49 +0200

    Top 10 Hacker News Stories

--- src/main/kotlin/com/fdtheroes/sgruntbot/handlers/message/HackerNews.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/handlers/message/HackerNews.kt
@@ -1,7 +1,7 @@
+package com.fdtheroes.sgruntbot.handlers.message
+
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fdtheroes.sgruntbot.BotConfig
-import com.fdtheroes.sgruntbot.handlers.message.HasHalp
-import com.fdtheroes.sgruntbot.handlers.message.MessageHandler
 import com.fdtheroes.sgruntbot.models.ActionResponse
 import com.fdtheroes.sgruntbot.utils.BotUtils
 import org.springframework.stereotype.Service
@@ -27,9 +27,9 @@ class HackerNews(botUtils: BotUtils, botConfig: BotConfig, private val mapper: O
 
     private fun fetchStory(id: Int): String {
         val story = botUtils.textFromURL("https://hacker-news.firebaseio.com/v0/item/$id.json?print=pretty")
-        val title = mapper.readTree(story).get("title").asText()
-        val url = mapper.readTree(story).get("url").asText()
-        return "- <a href=\"$url\">$title</a>"
+        val title = mapper.readTree(story)["title"].asText()
+        val url = mapper.readTree(story)["url"]?.asText() ?: "https://news.ycombinator.com/item?id=$id"
+        return "\uD83D\uDCE2 <a href=\"$url\">$title</a>"
     }
 
     override fun halp() = "<b>!hn</b> Top 10 Hacker News Stories"
