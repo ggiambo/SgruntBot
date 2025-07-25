c2ff70f547bbea45814b7e44c3b854de828a28c2
Giambo <ggiambo@gmail.com>
2025-07-25 21:51:12 +0200

    Precheck per request, non per handler!

--- src/main/kotlin/com/fdtheroes/sgruntbot/SgruntBot.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/SgruntBot.kt
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Service
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer
 import org.telegram.telegrambots.meta.api.objects.Update
+import java.time.LocalDateTime
+import kotlin.random.Random
 
 
 @Service
@@ -24,6 +26,7 @@ class SgruntBot(
 ) : LongPollingSingleThreadUpdateConsumer {
 
     private val log = LoggerFactory.getLogger(this.javaClass)
+    private val lastAuthorRegex = Regex("^!last\$", RegexOption.IGNORE_CASE)
 
     @PostConstruct
     fun postConstruct() {
@@ -42,9 +45,45 @@ class SgruntBot(
     }
 
     override fun consume(update: Update) {
+        if (!preCheck(update)) {
+            return
+        }
+
         handlers.forEach {
             CoroutineScope(Dispatchers.Default).launch { it.handle(update) }
         }
+    }
+
+    private fun preCheck(update: Update): Boolean {
+        if (botConfig.pausedTime != null) {
+            if (LocalDateTime.now() < botConfig.pausedTime) {
+                botConfig.pausedTime = null
+                log.info("Posso parlare di nuovo!")
+            } else {
+                return false
+            }
+        }
+
+        val message = update.message
+        if (message == null) {
+            return false
+        }
+
+        if (message.text.isNullOrEmpty()) {
+            return false
+        }
+
+        if (!lastAuthorRegex.containsMatchIn(message.text) && botUtils.isMessageInChat(message)) {
+            botConfig.lastAuthor = message.from
+        }
+
+        botConfig.pignolo = Random.nextInt(100) > 90
+
+        if (botUtils.getChatMember(message.from.id) == null) {
+            return false
+        }
+
+        return true
     }
 
     private fun publishNewVersionInfo() {
--- src/main/kotlin/com/fdtheroes/sgruntbot/handlers/message/MessageHandler.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/handlers/message/MessageHandler.kt
@@ -3,49 +3,14 @@ package com.fdtheroes.sgruntbot.handlers.message
 import com.fdtheroes.sgruntbot.BotConfig
 import com.fdtheroes.sgruntbot.handlers.Handler
 import com.fdtheroes.sgruntbot.utils.BotUtils
-import org.slf4j.LoggerFactory
 import org.telegram.telegrambots.meta.api.objects.Update
 import org.telegram.telegrambots.meta.api.objects.message.Message
-import java.time.LocalDateTime
-import kotlin.random.Random
 
 abstract class MessageHandler(protected val botUtils: BotUtils, protected val botConfig: BotConfig) : Handler {
 
-    private val log = LoggerFactory.getLogger(this.javaClass)
-    private val lastAuthorRegex = Regex("^!last\$", RegexOption.IGNORE_CASE)
-
     override suspend fun handle(update: Update) {
-        if (botConfig.pausedTime != null) {
-            if (LocalDateTime.now() < botConfig.pausedTime) {
-                botConfig.pausedTime = null
-                log.info("Posso parlare di nuovo!")
-            } else {
-                return
-            }
-        }
-
-        val message = update.message
-        if (message == null) {
-            return
-        }
-
-        if (message.text.isNullOrEmpty()) {
-            return
-        }
-
-        if (!lastAuthorRegex.containsMatchIn(message.text) && botUtils.isMessageInChat(message)) {
-            botConfig.lastAuthor = message.from
-        }
-
-        botConfig.pignolo = Random.nextInt(100) > 90
-
-        if (botUtils.getChatMember(message.from.id) == null) {
-            return
-        }
-
         handle(update.message)
     }
 
     abstract fun handle(message: Message)
-
 }
