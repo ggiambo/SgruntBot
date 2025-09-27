26231b27f5e62490da25674949b2fc10d1bb5eae
Giambo <ggiambo@gmail.com>
2025-09-27 15:40:18 +0200

    Niente "fortune" nel testing

--- src/test/kotlin/com/fdtheroes/sgruntbot/handlers/message/FortuneTest.kt
+++ src/test/kotlin/com/fdtheroes/sgruntbot/handlers/message/FortuneTest.kt
@@ -4,10 +4,14 @@ import com.fdtheroes.sgruntbot.BaseTest
 import com.fdtheroes.sgruntbot.models.ActionResponseType
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.spy
 
 internal class FortuneTest : BaseTest() {
 
-    private val fortune = Fortune(botUtils, botConfig)
+    private val fortune = spy(Fortune(botUtils, botConfig)) {
+        on { getFortune() } doReturn "Fake fortune, just for testing"
+    }
 
     @Test
     fun testPositive() {
