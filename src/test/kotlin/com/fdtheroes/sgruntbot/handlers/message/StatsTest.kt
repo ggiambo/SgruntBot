3d8cb292e70adad2b95aeccf86dc20a5484fc9d8
Giambo <ggiambo@gmail.com>
2026-05-19 21:18:57 +0200

    Codice stats più bellino, tests (#152)

--- src/test/kotlin/com/fdtheroes/sgruntbot/handlers/message/StatsTest.kt
+++ src/test/kotlin/com/fdtheroes/sgruntbot/handlers/message/StatsTest.kt
@@ -2,6 +2,7 @@ package com.fdtheroes.sgruntbot.handlers.message
 
 import com.fdtheroes.sgruntbot.BaseTest
 import com.fdtheroes.sgruntbot.Users
+import com.fdtheroes.sgruntbot.models.ActionResponseType
 import com.fdtheroes.sgruntbot.models.Stats
 import com.fdtheroes.sgruntbot.persistence.StatsService
 import com.fdtheroes.sgruntbot.utils.StatsUtil
@@ -9,6 +10,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
+import java.awt.TrayIcon
 import java.awt.image.BufferedImage
 import java.time.LocalDate
 import javax.imageio.ImageIO
@@ -37,7 +39,7 @@ internal class StatsTest : BaseTest() {
 
     @Test
     fun test_positive() {
-        val message = message("!stats")
+        val message = message("!stats g")
         stats.handle(message)
 
         assertThat(actionResponses).hasSize(1)
@@ -47,4 +49,46 @@ internal class StatsTest : BaseTest() {
         assertThat(image.width).isEqualTo(1280)
         assertThat(image.height).isEqualTo(1024)
     }
+
+    @Test
+    fun test_positive_uppercase() {
+        val message = message("!stats M")
+        stats.handle(message)
+
+        assertThat(actionResponses).hasSize(1)
+        val image = ImageIO.read(actionResponses.first().inputFile!!.newMediaStream)
+
+        assertThat(image.type).isEqualTo(BufferedImage.TYPE_3BYTE_BGR)
+        assertThat(image.width).isEqualTo(1280)
+        assertThat(image.height).isEqualTo(1024)
+    }
+
+    @Test
+    fun test_negative_with_help() {
+        val message = message("!stats xy")
+        stats.handle(message)
+
+        assertThat(actionResponses).hasSize(1)
+        assertThat(actionResponses.first().type).isEqualTo(ActionResponseType.Message)
+        assertThat(actionResponses.first().message).startsWith("<b>!stats <i>tipo</i></b>")
+    }
+
+    @Test
+    fun test_negative2() {
+        val message = message("!statsOH")
+        stats.handle(message)
+
+        assertThat(actionResponses).isEmpty()
+    }
+
+    @Test
+    fun test_negative_with_help2() {
+        val message = message("!stats L")
+        stats.handle(message)
+
+        assertThat(actionResponses).hasSize(1)
+        assertThat(actionResponses.first().type).isEqualTo(ActionResponseType.Message)
+        assertThat(actionResponses.first().message).startsWith("<b>!stats <i>tipo</i></b>")
+    }
+
 }
