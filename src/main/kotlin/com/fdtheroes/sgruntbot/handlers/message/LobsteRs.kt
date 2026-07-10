17136c761ceb33e0f49de8f5ab640827f491f720
Giambo <ggiambo@gmail.com>
2026-07-10 20:43:39 +0200

    Codice più compatto e illeggibile

--- src/main/kotlin/com/fdtheroes/sgruntbot/handlers/message/LobsteRs.kt
+++ src/main/kotlin/com/fdtheroes/sgruntbot/handlers/message/LobsteRs.kt
@@ -4,13 +4,12 @@ import com.fdtheroes.sgruntbot.BotConfig
 import com.fdtheroes.sgruntbot.models.ActionResponse
 import com.fdtheroes.sgruntbot.utils.BotUtils
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.meta.api.objects.message.Message
-import tools.jackson.databind.json.JsonMapper
 
 @Service
-class LobsteRs(botUtils: BotUtils, botConfig: BotConfig, private val jsonMapper: JsonMapper) :
-    MessageHandler(botUtils, botConfig), HasHalp {
+class LobsteRs(botUtils: BotUtils, botConfig: BotConfig) : MessageHandler(botUtils, botConfig), HasHalp {
 
     private val regex = Regex("^!lr$")
 
@@ -23,22 +22,26 @@ class LobsteRs(botUtils: BotUtils, botConfig: BotConfig, private val jsonMapper:
     }
 
     fun getMessageContent(): String {
-        val topTen = Jsoup.parse(botUtils.textFromURL("https://lobste.rs"))
+        return Jsoup.parse(botUtils.textFromURL("https://lobste.rs"))
             .select(".details")
             .take(10)
-        val urls = topTen.map {
-            val elem = it.select("a.u-url")
-            "\uD83E\uDD9E <a href=\"${elem.attr("href")}\">${elem.text()}</a>"
-        }
-        val comments = topTen.map {
-            val elem = it.select("span.comments_label a")
-            "<a href=\"https://lobste.rs/${elem.attr("href")}\">${elem.text()}</a>"
-        }
+            .map { Pair(getUrl(it), getComments(it)) }
+            .joinToString(
+                separator = "\n",
+                prefix = "Lobste.rs Top 10 Stories:\n"
+            ) { "<b>${it.first}</b> | ${it.second}" }
+    }
 
-        return urls.zip(comments).joinToString(
-            separator = "\n",
-            prefix = "Lobste.rs Top 10 Stories:\n"
-        ) { "<b>${it.first}</b> | ${it.second}" }
+    private fun getUrl(elem: Element): String {
+        return elem.expectFirst("a.u-url").let {
+            "\uD83E\uDD9E <a href=\"${it.attr("href")}\">${it.text()}</a>"
+        }
+    }
+
+    private fun getComments(elem: Element): String {
+        return elem.expectFirst("span.comments_label a").let {
+            "<a href=\"https://lobste.rs/${it.attr("href")}\">${it.text()}</a>"
+        }
     }
 
     override fun halp() = "<b>!lr</b> Top 10 Lobste.rs Stories"
